d1082242697e2a1fe082aa1e1fa70cbbd8ccc6c8
Niki Eskola <neskola@gmail.com>
2015-01-29 13:13:07 +0200

    calculating regular and evening ticks

--- src/fi/data/WorkShift.scala
+++ src/fi/data/WorkShift.scala
@@ -3,16 +3,16 @@ import java.util.Date
 import java.text.SimpleDateFormat
 
 class WorkShift(person : String, personId : String, shiftDate : String, shiftStart : String, shiftEnd: String) {
-  def p() = person
-  def pId() = personId
-  def sDate() = {
+  def p = person
+  def pId = personId
+  def sDate = {
     val dFormat = new SimpleDateFormat("dd.MM.yyyy")
     dFormat.parse(shiftDate)   
   }
-  def sStartHour() = shiftStart.split(":")(0).toInt
-  def sStartMinute() = shiftStart.split(":")(1).toInt
-  def sEndHour() = shiftEnd.split(":")(0).toInt
-  def sEndMinute() = shiftEnd.split(":")(1).toInt
+  def sStartHour = shiftStart.split(":")(0).toInt
+  def sStartMinute = shiftStart.split(":")(1).toInt
+  def sEndHour = shiftEnd.split(":")(0).toInt
+  def sEndMinute = shiftEnd.split(":")(1).toInt
   def shift = {
     sStartHour + ":" + sStartMinute + " - " + sEndHour + ":" + sEndMinute
   }
@@ -32,22 +32,31 @@ class WorkShift(person : String, personId : String, shiftDate : String, shiftSta
     var sHelper = sStartHour
     var eHelper = sEndHour
     
-    if (eHelper >= 18 || eHelper <=6) {
-      if (eHelper <=6) 
-        eHelper += 24
-    }
+    if (sEndHour <= 6 && sStartHour < 6) 
+      (((sEndHour - sStartHour) * 60) + (sEndMinute - sStartMinute)) / 15          
+    else if (sEndHour >= 18 && sStartHour <= 18) 
+      (((sEndHour - 18) * 60) + sEndMinute) / 15 
+    else if (sEndHour >= 18 && sStartHour > 18) 
+      (((sEndHour - sStartHour) * 60) + (sEndMinute - sStartMinute)) / 15
+    else if (sEndHour <= 6 && sStartHour >= 18) 
+      ((((sEndHour + 24)- sStartHour) * 60) + (sEndMinute - sStartMinute)) / 15
+    else if (sEndHour <= 6 && sStartHour >= 6 && sStartHour < 18) 
+      ((((sEndHour + 24)- 18) * 60) + (sEndMinute - sStartMinute)) / 15
+    else if (sStartHour < 6) 
+      (((6 - sStartHour) * 60) - sStartMinute) / 15
+    else 
+      0
   }
-
-  
   
   def regularPay = {
     (sTotalTicks * 3.75) / 4 
   }
   
   def eveningPay = {
-    
+    (sEveningTicks * 1.15) / 4
   }
   
   override def toString() = 
-    "" + p + " id = " + pId + " Date " + sDate + " shift " + shift + " ticks " + sTotalTicks + " regPay " + regularPay
+    "" + p + " id = " + pId + " Date " + sDate + " shift " + shift + " ticks " + sTotalTicks + " eveningTicks " + sEveningTicks + 
+    "\nRegularPay " + regularPay + " eveningPay " + eveningPay 
 }
